af4803498cfd70bce4d4f706321d49301b34c049
blessyparida <paridablessy@gmail.com>
2026-04-27 20:59:57 +0530

    Configure project for Heroku deployment

--- src/com/placeintel/util/DBUtil.java
+++ src/com/placeintel/util/DBUtil.java
@@ -5,13 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/placeintel_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Chocolate08!"; // Default placeholder password
+    private static final String LOCAL_URL = "jdbc:mysql://localhost:3306/placeintel_db";
+    private static final String LOCAL_USER = "root";
+    private static final String LOCAL_PASSWORD = "Chocolate08!";
 
     static {
         try {
-            // Load the MySQL JDBC driver
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
@@ -19,6 +18,10 @@ public class DBUtil {
     }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        if (dbUrl != null && !dbUrl.isEmpty()) {
+            return DriverManager.getConnection(dbUrl);
+        }
+        return DriverManager.getConnection(LOCAL_URL, LOCAL_USER, LOCAL_PASSWORD);
     }
 }
